4d71c32e6f2c50ebda703332109941818620ff08
Shin <tlsgustm12@gmail.com>
2023-12-16 21:53:21 +0900

    dynamic 추가

--- app/list/page.js
+++ app/list/page.js
@@ -1,10 +1,15 @@
 import {connectDB} from "@/util/database"
-import Link from "next/link"
 import ListItem from "./ListItem"
+
+export const dynamic = "force-dynamic"
 
 export default async function List() {
   const db = (await connectDB).db("forum");
   let res = await db.collection("post").find().toArray()
+  res = res.map((a) => {
+    a._id = a._id.toString()
+    return a
+  })
 
   return (
     <div className="list-bg">
